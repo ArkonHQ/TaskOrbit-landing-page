7fbc020ee9cb51a56becaddfe739efd1742dfabb
Arkon <balghbry77@gmail.com>
2026-04-16 17:08:59 +0300

    Done with feature's GSAP

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -13,6 +13,7 @@ const Features = () => {
   const animated = useRef(null);
 
   gsap.registerPlugin(ScrollTrigger);
+
   useGSAP(() => {
     const animatedText = document.querySelectorAll(".animatedText");
 
@@ -40,6 +41,55 @@ const Features = () => {
       { scope: animated },
     );
   });
+
+  useGSAP(() => {
+    const featureCards = document.querySelectorAll(".feature-card");
+
+    featureCards.forEach((featureCard) => {
+      gsap.fromTo(
+        featureCard,
+        {
+          opacity: 0,
+          y: 60,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          ease: "power2.out",
+          stagger: 0.2,
+          duration: 0.5,
+          scrollTrigger: {
+            trigger: featureCard,
+            start: "top 85%",
+            end: "top 50%",
+            scrub: 0.4,
+          },
+        },
+      );
+      const details = featureCard.querySelector(".feature-description");
+      if (!details) return;
+      gsap.set(details, { opacity: 0, y: 15 });
+      gsap.to(details, {
+        autoAlpha: 1,
+        y: 0,
+        duration: 0.4,
+        ease: "back.out(0.4)",
+        scrollTrigger: {
+          trigger: featureCard,
+          start: "center 60%",
+          end: "center 40%",
+          toggleActions: "play reverse play reverse",
+        },
+      });
+      const hoverIn = () =>
+        gsap.to(details, { autoAlpha: 1, y: 0, duration: 0.2 });
+      const hoverOut = () =>
+        gsap.to(details, { autoAlpha: 0, y: 15, duration: 0.1 });
+
+      featureCard.addEventListener("mouseenter", hoverIn);
+      featureCard.addEventListener("mouseleave", hoverOut);
+    });
+  }, []);
 
   return (
     <section
@@ -85,8 +135,9 @@ const Features = () => {
           <div className="md:col-span-2 justify-self-center flex flex-col">
             {FeaturesData.map((feature) => (
               <div
+                id={feature.id}
                 key={feature.id}
-                className="flex flex-row lg:flex-row gap-20 max-[1390px]:flex-col mb-50 items-center"
+                className="flex feature-card flex-row lg:flex-row gap-20 max-[1390px]:flex-col mb-50 items-center"
               >
                 <div className="video-card max-lg:w-100 max-lg:h-90 max-[1390px]:h-120 max-[1390px]:w-100 max-md:h-100 max-md:w-100">
                   <video
@@ -97,18 +148,18 @@ const Features = () => {
                     muted
                     playsInline
                   />
-                  <div className="absolute inset-0 bg-linear-to-t from-black/40 via-black/10 to-transparent" />
-                  <div className="video-overlay">
+                  <div className="absolute inset-0 bg-linear-to-t  from-black/40 via-black/10 to-transparent" />
+                  <div className="video-overlay feature-description">
                     <p className="text-start text-md text-white-50 text-shadow-2xs font-light">
                       {feature.overlayText}
                     </p>
                   </div>
                 </div>
                 <div className="bg-white/80 backdrop-blur-sm rounded-2xl max-[1390px]:-translate-x-14 drop-shadow-3xl shadow-black-100/40 max-[1024px]:translate-x-0 p-5 shadow-md w-80 shrink-0 hover:-translate-y-4 transition-all duration-200">
-                  <h3 className="text-xl text-black font-bold border-b border-gray-500 pb-2 mb-2">
+                  <h3 className="text-xl text-black font-bold border-b border-gray-500 pb-2 mb-2 feature-title ">
                     {feature.title}
                   </h3>
-                  <p className="text-gray-600">{feature.description}</p>
+                  <p className="text-gray-600  ">{feature.description}</p>
                 </div>
               </div>
             ))}
